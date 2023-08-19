2f6d10269aa4bc2375a6152f83e40619815b0d5e
ArtDev <artdevsemyonov@gmail.com>
2023-08-19 12:00:54 +0300

    add home page interface

---
+++ components/index.js
@@ -0,0 +1,18 @@
+import HomeHeader from "./HomeHeader";
+import FocusedStatusBar from "./FocusedStatusBar";
+import NFTCard from "./NFTCard";
+import { CircleButton, RectButton } from './Button';
+import { SubInfo, People, EndDate, EthPrice, NFTTitle } from "./SubInfo";
+
+export {
+    HomeHeader,
+    NFTCard,
+    SubInfo,
+    People,
+    EndDate,
+    RectButton,
+    CircleButton,
+    EthPrice,
+    NFTTitle,
+    FocusedStatusBar,
+}
---
+++ screens/Details.js
@@ -0,0 +1,11 @@
+import { Text, View, SafeAreaView } from 'react-native'
+
+const Details = () => {
+  return (
+    <SafeAreaView>
+        <Text>Details</Text>
+    </SafeAreaView>
+  )
+}
+
+export default Details
